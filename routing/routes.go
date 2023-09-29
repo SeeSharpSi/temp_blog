cf388f074478a2f6534563651e1eef9fdad7fad4
Silas <SilasTompkins@outlook.com>
2023-09-29 11:11:02 -0400

    git init, downsized

--- routing/routes.go
+++ routing/routes.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"text/template"
 )
 
 type Post struct {
@@ -22,82 +21,12 @@ func StartHandlers() *http.ServeMux {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", GetIndex)
 	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-	mux.HandleFunc("/home", GetHome)
-	mux.HandleFunc("/posts", GetPosts)
-	mux.HandleFunc("/pictures", GetPictures)
-	mux.HandleFunc("/form", GetForm)
-	mux.HandleFunc("/form_html", GetForm_html)
 	return mux
 }
 
 func GetIndex(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("got / request\n")
 	http.ServeFile(w, r, "tmpl/index.html")
-}
-
-func GetHome(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got /home request\n")
-	if r.Header.Get("Hx-Request") == "" {
-		http.Redirect(w, r, "/", http.StatusSeeOther)
-	} else {
-		http.ServeFile(w, r, "tmpl/home.html")
-	}
-}
-
-func GetPosts(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got /posts request\n")
-	tmpl, err := template.ParseFiles("tmpl/posts.html")
-	check(err)
-	Posts := []Post{
-		{
-			Title:   "title1",
-			Content: "content1",
-		},
-		{
-			Title:   "title2",
-			Content: "content2",
-		},
-		{
-			Title:   "title3",
-			Content: "content3",
-		},
-	}
-	if r.Header.Get("Hx-Request") == "" {
-		http.Redirect(w, r, "/", http.StatusSeeOther)
-	} else {
-		tmpl.Execute(w, Posts)
-	}
-}
-
-func GetForm_html(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got /form_html request\n")
-	if r.Header.Get("Hx-Request") == "" {
-		http.Redirect(w, r, "/", http.StatusSeeOther)
-	} else {
-		http.ServeFile(w, r, "tmpl/form.html")
-	}
-}
-
-func GetForm(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got /form request\n")
-	if r.Header.Get("Hx-Request") == "" {
-		http.Redirect(w, r, "/", http.StatusSeeOther)
-	} else {
-		fname := r.URL.Query().Get("fname")
-		lname := r.URL.Query().Get("lname")
-		fmt.Printf("\nfname => %s\nlname => %s\n", fname, lname)
-		_, err := w.Write([]byte("<button hx-get='/form_html' hx-swap='outerHTML'>New Form</button>"))
-		check(err)
-	}
-}
-
-func GetPictures(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got /pictures request\n")
-	if r.Header.Get("Hx-Request") == "" {
-		http.Redirect(w, r, "/", http.StatusSeeOther)
-	} else {
-		http.ServeFile(w, r, "tmpl/pictures.html")
-	}
 }
 
 func ServeStatic(w http.ResponseWriter, r *http.Request) {

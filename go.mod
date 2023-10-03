d8c0e1fdb3a9802d2812355839de2f42a884002e
Silas <SilasTompkins@outlook.com>
2023-10-03 13:17:13 -0400

    Added Air, templ

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module blog/main
 
 go 1.20
+
+require github.com/a-h/templ v0.2.364 // indirect
--- routing/routes.go
+++ routing/routes.go
@@ -1,6 +1,8 @@
 package routes
 
 import (
+	"blog/main/tmpl"
+	"context"
 	"fmt"
 	"io"
 	"net/http"
@@ -20,13 +22,20 @@ func check(e error) {
 func StartHandlers() *http.ServeMux {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", GetIndex)
+	mux.HandleFunc("/test", GetTest)
 	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	return mux
 }
 
 func GetIndex(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("got / request\n")
-	http.ServeFile(w, r, "tmpl/index.html")
+    http.ServeFile(w, r, "tmpl/index.html")
+}
+
+func GetTest(w http.ResponseWriter, r *http.Request) {
+    fmt.Printf("got /test request\n")
+    component := templ.Test("working")
+    component.Render(context.Background(), w)
 }
 
 func ServeStatic(w http.ResponseWriter, r *http.Request) {

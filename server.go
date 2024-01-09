7ba3177c8710d2b09beb780413db0f1c03670420
Silas <SilasTompkins@outlook.com>
2024-01-09 15:23:45 -0500

    added ip flag

--- server.go
+++ server.go
@@ -25,33 +25,34 @@ func check(e error) {
 
 func main() {
 	port := flag.Int("port", 9779, "port for localhost")
-	add_post := flag.Bool("ap", false, "add a post")
+	custom_ip := flag.String("ip", "127.0.0.1", "ip for host")
+	add_post := flag.Bool("p", false, "add a post")
 	flag.Parse()
 	if *add_post {
 		adding_post()
 	} else {
-		ip := "127.0.0.1:" + strconv.Itoa(*port)
+		ip := *custom_ip + ":" + strconv.Itoa(*port)
 		fmt.Printf("Server running on %s\n", ip)
-        mux := routes.StartHandlers()
-        server := http.Server{
-            Addr:    ip,
-            Handler: mux,
-        }
-        c := make(chan os.Signal, 1)
-        signal.Notify(c,os.Interrupt)
-        go func() {
-            s := <-c
-            server.Close()
-            fmt.Println(s)
-        }()
-        err := server.ListenAndServe()
-        defer server.Close()
-        if errors.Is(err, http.ErrServerClosed) {
-            fmt.Printf("server closed\n")
-        } else if err != nil {
-            fmt.Printf("error starting server: %s\n", err)
-            os.Exit(1)
-        }
+		mux := routes.StartHandlers()
+		server := http.Server{
+			Addr:    ip,
+			Handler: mux,
+		}
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, os.Interrupt)
+		go func() {
+			s := <-c
+			server.Close()
+			fmt.Println(s)
+		}()
+		err := server.ListenAndServe()
+		defer server.Close()
+		if errors.Is(err, http.ErrServerClosed) {
+			fmt.Printf("server closed\n")
+		} else if err != nil {
+			fmt.Printf("error starting server: %s\n", err)
+			os.Exit(1)
+		}
 	}
 }
 
@@ -67,12 +68,12 @@ func adding_post() {
 	teaser = strings.Replace(teaser, "\n", "", -1)
 	fmt.Println("Content (relative cli location): ")
 	content_file_location, err := reader.ReadString('\n')
-    content_file_location = content_file_location[:len(content_file_location)-1]
-    file, err := os.ReadFile(content_file_location)
-    check(err)
-    var buf bytes.Buffer
-    err = goldmark.Convert(file, &buf)
-    check(err)
-    content := buf.String()
+	content_file_location = content_file_location[:len(content_file_location)-1]
+	file, err := os.ReadFile(content_file_location)
+	check(err)
+	var buf bytes.Buffer
+	err = goldmark.Convert(file, &buf)
+	check(err)
+	content := buf.String()
 	sql_funcs.Add_Post(title, teaser, content)
 }
